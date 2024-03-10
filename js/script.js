9bece9dd973c2b9902c4b080c51e4448bbff5e0b
akakinozadze <145987347+akakinozadze@users.noreply.github.com>
2024-03-10 17:04:17 +0400

    test

--- js/script.js
+++ js/script.js
@@ -11,9 +11,52 @@ const menu1 = document.getElementById("menu1");
 const NavBar = document.getElementById("NavBar");
 menu1.addEventListener("click", function () {
   NavBar.classList.toggle("navBar2");
+  inputSearch1.classList.remove("newSearch2");
 });
 // close
 const close2 = document.getElementById("close2");
 close2.addEventListener("click", function () {
   NavBar.classList.remove("navBar2");
 });
+//li
+const li1 = document.querySelectorAll(".garmon");
+
+li1.forEach(function (item) {
+  item.addEventListener("click", function () {
+    this.classList.toggle("garmon1");
+  });
+});
+
+const ulBox = document.querySelectorAll(".ulBox");
+
+ulBox.forEach(function (element) {
+  element.addEventListener("click", function () {
+    element.classList.add("newUlBox");
+  });
+  console.log(element);
+});
+// console.log(ulBox);
+// const ulBox = document.querySelector(".ulBox");
+
+// ulBox.addEventListener("click", function () {
+//   ulBox.classList.add("newUlBox");
+// });
+
+// const ulBox = document.querySelectorAll(".ulBox");
+
+// ulBox.addEventListener("click", function () {
+//   ulBox.classList.add("newUlBox");
+// });
+// ulBox.forEach(function (item) {
+//   item.addEventListener("click", function () {
+//     this.classList.toggle("garmon2");
+//   });
+// });
+const iconBack = document.getElementById("icon_back");
+
+
+
+// iconBack.addEventListener("click", function (e) {
+//   e.stopPropagation();
+//   ulBox.classList.remove("newUlBox");
+// });
